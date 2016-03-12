3e277904bff99b3a595e6c76af5590609b9330d1
Mikko Mattila <mikko.mattila@iki.fi>
2016-03-12 21:38:58 +0200

    Fixed bug in linechart()

--- chart/views.py
+++ chart/views.py
@@ -10,6 +10,7 @@ def linechart(request):
     tooltip_date = "%d %b %Y %H:%M:%S %p"
     extra_serie = {"tooltip": {"y_start": "There are ", "y_end": " calls"},
                    "date_format": tooltip_date}
+
     template_chartdata = {
         'x': [],
         'model_column1': "bedroom_temperature", #model field name
@@ -23,27 +24,30 @@ def linechart(request):
     for key, val in template_chartdata.iteritems():
         chartdata[key] = val
         if key == "model_column%s" %(str(i), ):
-            series[key] = chartdata["model_column%s" %(str(i), )]
-            chartdata["name%s" % (str(i), )] = DataPoint._meta.get_field(series[key]).verbose_name
+            series["y%s" % (str(i), )] = chartdata["model_column%s" %(str(i), )]
+            chartdata["name%s" % (str(i), )] = DataPoint._meta.get_field(template_chartdata[key]).verbose_name
             chartdata["y%s" % (str(i), )] = []
             chartdata["extra%s" % (str(i), )] = extra_serie
             i += 1
 
+    print series
     print chartdata
     points = DataPoint.objects.order_by('datetime')
     if len(points) > 0:
         for point in points:
             point_epoch_ms = int(time.mktime(point.datetime.timetuple())) * 1000
+            #Check that the point has all the data points being plotted.
+            #If not, skip the data point.
             point_ok = True
             for key, val in chartdata.iteritems():
                 if "model_column" in key and not point._meta.get_field(val):
                     point_ok = False
-            if point_ok:
+            if point_ok: #OK to add datapoint to chart and all series.
                 chartdata["x"].append(point_epoch_ms)
                 for key, val in series.iteritems():
                     print "%s : %s: %s" %(key, val, getattr(point, val))
                     if True:
-                        print chartdata[key]
+                        print series[key] + " " + str(getattr(point, val))
                         chartdata[key].append(getattr(point, val))
 
     data = {
